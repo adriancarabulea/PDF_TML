71681e275eb7b6b8f03c07b1fb3543d27484c320
Adrian Carabulea <adriancarabulea@yahoo.com>
2025-10-22 13:22:02 +0300

    The first change

--- PDF_TML/PDF_TML/Form1.Designer.cs
+++ PDF_TML/PDF_TML/Form1.Designer.cs
@@ -88,6 +88,7 @@
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(173, 20);
             this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "Test";
             // 
             // button4
             // 
